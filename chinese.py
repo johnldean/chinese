92a2c8471b969b40818caf54df33bce95e11cfce
John Dean <deanjl@stanford.edu>
2018-12-06 02:54:51 -0800

    bout to get rekt by a midterm

--- chinese.py
+++ chinese.py
@@ -4,8 +4,28 @@ import os
 def shift_deck(fn):
 	f = open(fn,"r")
 	for l in f.readlines():
-		p1 = l.index('(')
 		c = l.index(',')
-		print(l[c+1:p1-1] + "," + l[:c] + " " +l[p1:],end='')
+		if "(" in l:
+			p1 = l.index('(')
+			print(l[c+1:p1-1] + "," + l[:c] + " " +l[p1:],end='')
+		else:
+			print(l[c+1:-1] + "," + l[:c])
 
+for d in os.walk('words/'):
+	for f in d[2]:
+		shift_deck("words/"+f)
+#shift_deck("words/A5_2.txt")
+
+
+'''
+f = open("words/A5_2(raw).txt")
+f2 = open("words/A5_2(u).txt","w")
+while True:
+	l1 = f.readline()
+	l2 = f.readline()
+	if not l2:
+		break 
+	l1 = l1.replace("*","")
+	f2.write(l1[:-1] +" "+ l2)
+'''
 
