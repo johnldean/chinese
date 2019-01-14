d3423518782a7f8fa4761d9caf6dcc244ac73911
John Dean <deanjl@stanford.edu>
2019-01-14 00:22:04 -0800

    more units

--- chinese.py
+++ chinese.py
@@ -11,21 +11,36 @@ def shift_deck(fn):
 		else:
 			print(l[c+1:-1] + "," + l[:c])
 
-for d in os.walk('words/'):
-	for f in d[2]:
-		shift_deck("words/"+f)
-#shift_deck("words/A5_2.txt")
+def shift_2(fn):
+	f = open(fn,"r")
+	for l in f.readlines():
+		c = l.index(',')
+		if "(" in l:
+			p1 = l.index('(')
+			print(l[c+1:p1-1] + l[:c] +","  + " " +l[p1:],end='')
+		else:
+			print(l[c+1:-1] + "," + l[:c])
+
+def concat(): 
+	for d in os.walk('words/'):
+		for f in d[2]:
+			for l in open("words/"+f,"r").readlines():
+				print(l,end="")
+			print("")
+
+	#shift_deck("words/"+f)
+	#shift_deck("words/A5_2.txt")
 
 
-'''
-f = open("words/A5_2(raw).txt")
-f2 = open("words/A5_2(u).txt","w")
-while True:
-	l1 = f.readline()
-	l2 = f.readline()
-	if not l2:
-		break 
-	l1 = l1.replace("*","")
-	f2.write(l1[:-1] +" "+ l2)
-'''
+def proc(n):
+	f = open("words/"+n+"(raw).txt")
+	f2 = open("words/"+n+".txt","w")
+	while True:
+		l1 = f.readline()
+		l2 = f.readline()
+		if not l2:
+			break 
+		l1 = l1.replace("*","")
+		f2.write(l1[:-1] +" "+ l2)
 
+proc("A6_2")
